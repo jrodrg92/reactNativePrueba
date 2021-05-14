a51a0a481de8de363ee0c9060daba3b98c8dae37
Javier Rodríguez González <jrodrg92@gmail.com>
2021-05-14 16:21:14 +0200

    Initial commit

---
+++ windows/criptovikreactnative/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::criptovikreactnative::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::criptovikreactnative::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
